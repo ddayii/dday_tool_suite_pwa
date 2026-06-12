66338fb00a0c0178be6c6ed1afccd37be2a7981f
ddaycontrols@gmail.com <##Kjp*#01k7v1tn%>
2026-06-12 18:50:35 -0400

    Initial commit: DDay Controls PWA

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE = 'dday-controls-v6';
+const ASSETS = [
+  './',
+  './index.html',
+  './scalar.html',
+  './unit-converter.html',
+  './ascii-chart.html',
+  './style.css',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  './icons/converter-icon.png',
+  './icons/ascii-icon.png',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
